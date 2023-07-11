ce981285b16891e8c6d99448a1ece1587ac7bc8d
Tarun Singh <taruninsa1@gmail.com>
2023-07-10 23:50:40 -0400

    Added hash based compare logic to update file

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ import { uploadToS3 } from "./cloudflare";
 
 http("index", async (req, res) => {
   const data = await getFilteredPRs("tarun7singh");
-  await uploadToS3(data);
-  res.send("success");
+  const response = await uploadToS3(data);
+  res.send(response);
 });
